13575402936e40b8b73dc9ada74c9ec755a9c361
Janek <janek@cerber.network>
2024-10-10 22:23:36 +0200

    Dodanie docker-compose posiadający silnik php oraz nginx

--- app/index.php
+++ app/index.php
@@ -1,1 +1,17 @@
-Początek naszej aplikacji : )
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Put Notes</title>
+    <link href="/plugins/bootstrap/bootstrap.min.css">
+    <script src="/plugins/bootstrap/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <?php echo "Test"; ?>
+        </div>
+    </div>
+</body>
+</html>
