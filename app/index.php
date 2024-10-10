9c1f153386841be0053b2e1d7d9f9b5a2f352690
Janek <janek@cerber.network>
2024-10-10 22:37:10 +0200

    Dodanie rel=styleshhet

--- app/index.php
+++ app/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Put Notes</title>
-    <link href="/plugins/bootstrap/bootstrap.min.css">
+    <link href="/plugins/bootstrap/bootstrap.min.css" rel="stylsheet">
     <script src="/plugins/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
 <body>
